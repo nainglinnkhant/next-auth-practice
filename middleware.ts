8d09872d07a9f4a0f934939949213b504fd10547
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-06-14 18:35:48 +0630

    feat: add middleware for route protection

---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req })
+    const isAuth = !!token
+    const isAuthPage = req.nextUrl.pathname === '/'
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/protected', req.url))
+      }
+
+      return null
+    }
+
+    if (!isAuth) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true
+      },
+    },
+  }
+)
+
+export const config = {
+  matcher: ['/', '/protected'],
+}
